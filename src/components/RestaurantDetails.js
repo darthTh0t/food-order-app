c0f81193c6bfb8a30ed979b9cbadb416b46a5915
arnav-sahoo <arnav.sahoo.97@gmail.com>
2023-02-21 14:21:00 +0530

    added async API call, optimised details comp

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./styles/RestaurantDetails.css";
 
-const RestaurantDetails = () => {
+const RestaurantDetails = (props) => {
   return (
-    <div className="restComp">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.4 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.7, type: "spring" }}
+      whileHover={{ scale: 1.05, cursor: "pointer" }}
+      className="restComp"
+    >
       <h1>Image</h1>
-      <h3>Name</h3>
-      <p>Time Taken</p>
-    </div>
+      <h3>{props.name}</h3>
+      <p>{props.time}</p>
+    </motion.div>
   );
 };
 
--- src/pages/RestaurantPage.js
+++ src/pages/RestaurantPage.js
@@ -1,18 +1,35 @@
-import { lazy, React } from "react";
+import { lazy, React, useEffect, useState } from "react";
 import "./styles/RestaurantPage.css";
 
-const LazyRestComp = lazy(() => import("../components/RestaurantDetails"));
+const LazyRestComp = lazy(() => import("../components/RestaurantDetails")); //lazy loading
+
 const RestaurantPage = () => {
+  const REST_DET_URL = "http://localhost:5500/content";
+
+  const [details, setDetails] = useState([]);
+
+  //load the details when the page loads
+  useEffect(() => {
+    async function fetchDetails() {
+      const response = await fetch(REST_DET_URL);
+      const getDetails = await response.json();
+      setDetails(getDetails);
+    }
+    fetchDetails();
+  }, []);
+
   return (
     <>
       <div className="rootDiv">
         Restaurant Page
         <div className="restDet">
-          <LazyRestComp />
-          <LazyRestComp />
-          <LazyRestComp />
-          <LazyRestComp />
-          <LazyRestComp />
+          {details.map((result) => (
+            <LazyRestComp
+              key={result.id}
+              name={result.name}
+              time={result.time}
+            />
+          ))}
         </div>
       </div>
     </>
